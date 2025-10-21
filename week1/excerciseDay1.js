ac2a4deed7ee7563969c6436d37fb9e3b8b99329
Sushant Shrestha <sushant@kali.kali>
2025-10-21 01:44:23 +0100

    excercise for day 1

---
+++ week1/excerciseDay1.js
@@ -0,0 +1,6 @@
+const name = "Sushant Shrestha";
+const age = 24;
+const language = "JavaScript";
+
+
+console.log(`Hello, My name is ${name}, I am ${age} years old and I am learning ${language}.`);
